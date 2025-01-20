fd85be6f635ba41383b32f8c64c835c52573bfcd
AvanMyers <smail1341@gmail.com>
2025-01-20 14:10:28 +0300

    create

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BracketExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bracket = 0;
+            int deep = 0;
+            int bracketEndResult = 0;
+            string bracketExpression = "";
+
+            Console.WriteLine("Введите скобочное выражение: ");
+            bracketExpression = Console.ReadLine();
+
+            for (int i = 0; i < bracketExpression.Length; i++)
+            {
+                if (bracketExpression[i] == '(')
+                    bracket++;
+                else if (bracketExpression[i] == ')')
+                    bracket--;
+
+                if (bracket == bracketEndResult && bracketExpression[i] != ')')
+                    bracket--;
+
+                if (bracket > deep)
+                    deep = bracket;
+            }
+
+            if (bracketExpression[0] != '(' || bracketExpression[bracketExpression.Length - 1] != ')' || bracket != bracketEndResult)
+                Console.WriteLine($"{bracketExpression} строка не корректна");
+            else Console.WriteLine($"{bracketExpression} строка корректная и максимум глубины равняется {deep}.");
+
+            Console.ReadKey();
+        }
+    }
+}

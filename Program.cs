3631adf4430743e8c029f350b0d003ee8caf8ada
AvanMyers <smail1341@gmail.com>
2025-01-21 10:57:35 +0300

    edit

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace BracketExpression
     {
         static void Main(string[] args)
         {
-            int bracket = 0;
             int deep = 0;
+            int maxDeep = 0;
+            char bracketLeft = ')';
+            char bracketRight = '(';
             int bracketEndResult = 0;
             string bracketExpression = "";
 
@@ -16,21 +18,23 @@ namespace BracketExpression
 
             for (int i = 0; i < bracketExpression.Length; i++)
             {
-                if (bracketExpression[i] == '(')
-                    bracket++;
-                else if (bracketExpression[i] == ')')
-                    bracket--;
+                if (bracketExpression[i] == bracketRight)
+                    deep++;
+                else if (bracketExpression[i] == bracketLeft)
+                    deep--;
 
-                if (bracket == bracketEndResult && bracketExpression[i] != ')')
-                    bracket--;
+                if (deep < 0)
+                {
+                    break;
+                }
 
-                if (bracket > deep)
-                    deep = bracket;
+                    if (deep > maxDeep)
+                        maxDeep = deep;
             }
 
-            if (bracketExpression[0] != '(' || bracketExpression[bracketExpression.Length - 1] != ')' || bracket != bracketEndResult)
-                Console.WriteLine($"{bracketExpression} строка не корректна");
-            else Console.WriteLine($"{bracketExpression} строка корректная и максимум глубины равняется {deep}.");
+            if (deep == 0)
+                Console.WriteLine($"{bracketExpression} строка корректная и максимум глубины равняется {maxDeep}.");
+            else Console.WriteLine($"{bracketExpression} строка не корректна");
 
             Console.ReadKey();
         }
